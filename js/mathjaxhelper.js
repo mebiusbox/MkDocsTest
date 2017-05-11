a712c41712c0161f220fc1729ee7cabf18e52d1c
mebiusbox <flare.mebius@gmail.com>
2017-05-12 03:16:35 +0900

    Deployed 4537352 with MkDocs version: 0.16.3

---
+++ js/mathjaxhelper.js
@@ -0,0 +1,11 @@
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [['$$','$$'], ['\[','\]']],
+    processEscapes: true,
+    processEnvironments: true,
+    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+    TeX: { equationNumbers: { autoNumber: "AMS" },
+         extensions: ["AMSmath.js", "AMSsymbols.js"] }
+  }
+});
