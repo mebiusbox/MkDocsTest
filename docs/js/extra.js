fbb68893c765b1369d6b5cd32d8b95ecc1499dfb
mebiusbox <flare.mebius@gmail.com>
2019-03-30 22:26:13 +0900

    mkdocs 1.0.4, mkdocs-material 4.1.1

---
+++ docs/js/extra.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [['$$','$$'], ['\\[','\\]']],
+    processEscapes: true
+  },
+  "HTML-CSS": {
+    availableFonts: [],
+    preferredFont: null,
+    webFont: "STIX-Web"
+  }
+};
